ecd3331e4b7ac60a48d1b28a0d02912596caae38
slorello89 <slorello89@gmail.com>
2023-06-23 16:49:51 -0400

    cleaning up README/some path names

--- examples/tls/run-tls.sh
+++ examples/tls/run-tls.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 sh gen-certs.sh
-chmod +x ./certs/ca.crt ./certs/redis.crt ./certs/redis.key
+chmod +r ./certs/ca.crt ./certs/redis.crt ./certs/redis.key
+cd ..
+sh ./fetch_model.sh
+cd tls
 docker-compose build
 docker-compose up
