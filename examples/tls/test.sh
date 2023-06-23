b0fce3105c306ac8000a1c413fdefed715f6aaaf
slorello89 <slorello89@gmail.com>
2023-06-23 11:59:29 -0400

    pulling in model binaries

--- examples/tls/test.sh
+++ examples/tls/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sleep 20
+sleep 5
 /workspace/install/bin/image_client -u http://triton-server:8000 -m densenet_onnx -c 3 -s INCEPTION /workspace/images/mug.jpg
 numKeys=$(redis-cli -h triton-redis --tls --cert /certs/redis.crt --key /certs/redis.key --cacert /certs/ca.crt DBSIZE) # check that there's only one key
 
